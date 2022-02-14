830cbddebf5ae07d4113c5b7b30bca2f3e2f8cbb
rivalnofirm <rivalnof2@gmail.com>
2022-02-14 20:24:19 +0700

    update service access token

--- src/domain/access_token/service.go
+++ src/domain/access_token/service.go
@@ -1,6 +1,9 @@
 package access_token
 
-import "github.com/rivalnofirm/bookstore_oauth-api/src/src/utils/errors"
+import (
+	"github.com/rivalnofirm/bookstore_oauth-api/src/src/utils/errors"
+	"strings"
+)
 
 type Repository interface {
 	GetById(string) (*AccessToken, *errors.RestErr)
@@ -21,6 +24,10 @@ func NewService(repo Repository) Service {
 }
 
 func (s *service) GetById(accessTokenId string) (*AccessToken, *errors.RestErr) {
+	accessTokenId = strings.TrimSpace(accessTokenId)
+	if len(accessTokenId) == 0 {
+		return nil, errors.NewBadRequestError("invalid access token id")
+	}
 	accessToken, err := s.repository.GetById(accessTokenId)
 	if err != nil {
 		return nil, err
